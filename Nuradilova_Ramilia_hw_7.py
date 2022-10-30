94092e0b8257923e7335ae59f7050b91fe5df55d
Ram1lia <116273468+Ram1lia@users.noreply.github.com>
2022-10-30 17:05:29 +0600

    'hw8'

--- Nuradilova_Ramilia_hw_7.py
+++ Nuradilova_Ramilia_hw_7.py
@@ -22,3 +22,21 @@ def bubble_sort(lst):
             if lst[i] < lst[i - 1]:
                 lst[i - 1], lst[i] = lst[i], lst[i - 1]
     return lst
+
+
+    def create_products(conn):
+        create_product(conn, ('dirol', 35.50, 5))  # 1
+        create_product(conn, ('гномик', 25.00, 10))  # 2
+        create_product(conn, ('кола 2л', 120.00, 8))  # 3
+        create_product(conn, ('NITRO', 62.40, 12))  # 4
+        create_product(conn, ('хлеб', 20, 5))  # 5
+        create_product(conn, ('тоналка', 600.99, 2))  # 6
+        create_product(conn, ('Мыло', 108.60, 7))  # 7
+        create_product(conn, ('mascakara', 400, 20))  # 8
+        create_product(conn, ('lipstick', 250.00, 6))  # 9
+        create_product(conn, ('mackbook', 105000.00, 3))  # 10
+        create_product(conn, ('Alpen Gold', 114.59, 4))  # 11
+        create_product(conn, ('water', 19.40, 8))  # 12
+        create_product(conn, ('Моющее средство', 73.70, 4))  # 13
+        create_product(conn, ('Порошок', 250.00, 3))  # 14
+        create_product(conn, ('Подсолнечное Масло', 240.00, 5))  # 15
